4d21e38dd497c473517389367d5431fa504b8e02
Tyler Grantham <tjaygrantham@gmail.com>
2020-05-27 11:32:51 -0400

    solve using array methods

--- homework.js
+++ homework.js
@@ -5,18 +5,36 @@
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove(arr, obj){
+    return arr.filter(name => {
+        return name !== obj;
+    });
+}
 
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(arr){
+    return arr.length > 0 ? arr.reduce((acc, cur) => {
+        return acc + cur;
+    }) : 0;
+}
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(arr){
+    return arr.length > 0 ? sum(arr)/arr.length : undefined;
+}
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum(arr){
+    return arr.length > 0 ? arr.reduce((acc, cur) => {
+        return acc < cur ? acc : cur;
+    }) : undefined;
+}
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -41,9 +59,19 @@
 // Note 2: Selection sort can be implemented using one array. Read the explanation at
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
+function selectionSort(arr){
+    return arr.sort((a, b) =>{
+        return a - b;
+    });
+}
 
 // 7. Create a function called `textList` that takes an array and joins its elements
 // into a string separated by commas.
 //
 // For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
 // `"Cadence,Ordel,Marion"`.
+function textList(arr){
+    return arr.length > 0 ? arr.reduce((acc, cur) =>{
+        return `${acc},${cur}`;
+    }) : '';
+}
